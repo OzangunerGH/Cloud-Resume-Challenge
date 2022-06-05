8f039acc4bda2c56292e7ab8c8219e21b17d493a
Ozan Guner <85585666+OzangunerGH@users.noreply.github.com>
2022-06-06 01:13:16 +0300

    Update counter.js

--- frontend/static/js/counter.js
+++ frontend/static/js/counter.js
@@ -17,13 +17,16 @@
 // GET API REQUEST
 async function get_visitors() {
     // call post api request function
-    // await post_visitor();
+    //await post_visitor();
     try {
         let response = await fetch('https://ahwlkl4zp8.execute-api.us-east-1.amazonaws.com/prod/visitor_count', {
             method: 'GET',
+            headers: {
+                //'x-api-key': 'ImqKZvELgw5TRETsNBXlM9MP7D0sUEvn7Jg1RrPA',
+            }
         });
         let data = await response.json()
-        document.getElementById("visitor_count").innerHTML = data.body
+        document.getElementById("visitor_count").innerHTML = data.body;
         console.log(data);
         return data;
     } catch (err) {
