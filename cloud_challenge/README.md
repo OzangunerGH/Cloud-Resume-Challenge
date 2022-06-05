2c3a0d50387a4159354964515506d5a25329bbc3
Ozan Guner <85585666+OzangunerGH@users.noreply.github.com>
2022-06-05 20:42:21 +0300

    Add files via upload

---
+++ cloud_challenge/README.md
@@ -0,0 +1,2 @@
+# Cloud Resume Challenge
+Cloud Resume Challenge - Front End & Back End Repository for CI/CD
---
+++ cloud_challenge/tests/test.py
@@ -0,0 +1,25 @@
+from getFunction import app
+import os
+
+def test_lambda_handler():
+    # Checking AWS Credentials
+    assert "AWS_ACCESS_KEY_ID" in os.environ
+    assert "AWS_SECRET_ACCESS_KEY" in os.environ
+    response = app.lambda_handler("", "")
+    response_2 = app.lambda_handler("", "")
+
+    # assert return keys
+    assert "statusCode" in response
+    assert "headers" in response
+    assert "body" in response
+
+    # checking headers for CORS
+    assert "Access-Control-Allow-Origin" in response["headers"]
+    assert "Access-Control-Allow-Methods" in response["headers"]
+    assert "Access-Control-Allow-Headers" in response["headers"]
+
+    # checking status code
+    assert response["statusCode"] == 200
+    assert response_2["body"] == response["body"] + 1
+        
+test_lambda_handler()
