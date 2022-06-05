e7a90f515094bcda3b92e5055d27b284d7c92cff
Ozan Guner <85585666+OzangunerGH@users.noreply.github.com>
2022-06-05 14:59:45 +0300

    Update app.py

--- getFunction/app.py
+++ getFunction/app.py
@@ -12,7 +12,7 @@ def lambda_handler(event, context):
         Key={'id': 'count'},
         UpdateExpression='SET visitor_count = visitor_count + :val1',
         ExpressionAttributeValues={
-            ':val1': 3
+            ':val1': 1
         },
         ReturnValues="UPDATED_NEW"
     )
