16dbfe46f055c390722b5433bfff4d360061a7d7
Ozan Guner <85585666+OzangunerGH@users.noreply.github.com>
2022-06-05 13:03:55 +0300

    Create test.py

---
+++ getFunction/test.py
@@ -0,0 +1,26 @@
+import app
+
+
+
+def test_lambda_handler():
+    # Checking AWS Credentials
+    assert "AWS_ACCESS_KEY_ID" in os.environ
+    assert "AWS_SECRET_ACCESS_KEY" in os.environ
+    response = app.lambda_handler("", "")
+    response_2 = app.lambda_handler("", "")
+
+    # assert return keys
+    assert "statusCode" in response
+    assert "headers" in response
+    assert "body" in response
+
+    # checking headers for CORS
+    assert "Access-Control-Allow-Origin" in response["headers"]
+    assert "Access-Control-Allow-Methods" in response["headers"]
+    assert "Access-Control-Allow-Headers" in response["headers"]
+
+    # checking status code
+    if response["statusCode"] == 200:
+        if response_2["body"] == response["body"] + 1:
+            print("Lambda Function passed the test successfully.")
+            return
