fd01798984555ae2bad831966437fe8ec15263b0
Ozan Guner <85585666+OzangunerGH@users.noreply.github.com>
2022-07-02 01:15:12 +0300

    Add files via upload

--- cloud_challenge/frontend/static/js/counter.js
+++ cloud_challenge/frontend/static/js/counter.js
@@ -1,6 +1,6 @@
 //async function post_visitor() {
 //    try {
-//        let response = await fetch('https://9hxxfa923c.execute-api.us-east-1.amazonaws.com/prod/visitor_count', {
+//        let response = await fetch('https://ahwlkl4zp8.execute-api.us-east-1.amazonaws.com/prod/visitor_count', {
 //            method: 'POST',
 //            headers: {
 //                //'x-api-key': '91P74Jtwbq5YJLJBJTElz6KxTXYotead9YQY9EvM'
@@ -17,16 +17,16 @@
 // GET API REQUEST
 async function get_visitors() {
     // call post api request function
-    // await post_visitor();
+    //await post_visitor();
     try {
-        let response = await fetch('https://9hxxfa923c.execute-api.us-east-1.amazonaws.com/prod/visitor_count', {
+        let response = await fetch('https://ahwlkl4zp8.execute-api.us-east-1.amazonaws.com/prod/visitor_count', {
             method: 'GET',
             headers: {
-                //'x-api-key': '91P74Jtwbq5YJLJBJTElz6KxTXYotead9YQY9EvM',
+                //'x-api-key': 'ImqKZvELgw5TRETsNBXlM9MP7D0sUEvn7Jg1RrPA',
             }
         });
         let data = await response.json()
-        document.getElementById("visitor_count").innerHTML = data.body
+        document.getElementById("visitor_count").innerHTML = data;
         console.log(data);
         return data;
     } catch (err) {
